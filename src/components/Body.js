63a2565018d8bba8a2caa493ce5b8e9a564e6c59
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-10-03 12:55:01 -0700

    building body of portfolio and linking all pages to body.js

---
+++ src/components/Body.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ currentPage, handlePageChange }) {
     return (
       <header>
         <div>
-          <h1>name</h1>
+          <h1>Zachariah "Casper" Go</h1>
           <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
         </div>
       </header>
