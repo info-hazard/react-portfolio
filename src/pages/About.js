a56e9f144062ee45073abfc698714286053406da
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-10-03 14:19:54 -0700

    filled about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,10 +4,11 @@ import '../styles/style.css';
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p>
-
-      </p>
+      <img className="avi" src="avi.jpg" alt="Avatar" style={{width: "400px"}} />
+      <h1>About me</h1>
+      <p> I am an in-training full stack software developer! I am experienced in using MERN as well as SQL based full stack applications.</p>
+      <p> I believe there is always room for improvement, this portfolio is very much a work in progress!</p>
+      <p> You are witnessing a barebones portfolio but as I grow it will grow with me! </p>
     </div>
   );
 }
