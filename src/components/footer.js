f69be9f1e590d46523b924299b045a6bba4d8fc6
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-10-03 16:37:13 -0700

    finishing touches

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <nav>
+        <ul className="nav d-flex justify-content-center">
+          <li className="nav-item">
+            <a href="https://github.com/info-hazard" className="nav-link">
+            <img src="github.png" alt="GitHub" width="45" height="45" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
