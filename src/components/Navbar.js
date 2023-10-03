fcc21299f6ad1e85ae151d8ace6246dc7499f8d8
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-10-03 02:29:45 -0700

    added functionality to navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+function Navbar({ currentPage, handlePageChange }) {
+       return (
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a
+              href="#About me"
+              onClick={() => handlePageChange('About')}
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+               className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      );
+    }
+
+export default Navbar;
